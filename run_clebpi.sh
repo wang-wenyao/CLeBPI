195658d93b9d26f2ca60a00ddd58ccda0ac0e62b
wang-wenyao <101500084+wang-wenyao@users.noreply.github.com>
2023-05-15 19:17:53 +0200

    Add files via upload

---
+++ run_clebpi.sh
@@ -0,0 +1,15 @@
+CUDA_LAUNCH_BLOCKING=1 python run_clebpi.py \
+  --model_name_or_path <path2CLeBPI> \
+  --train_file datasetPath/priority_train.csv \
+  --validation_file datasetPath/priority_valid.csv \
+  --test_file datasetPath/priority_test.csv \
+  --cache_dir ./cache_dir \
+  --do_train \
+  --do_eval \
+  --do_predict \
+  --max_seq_length 64 \
+  --per_device_train_batch_size 16 \
+  --learning_rate 5e-6 \
+  --num_train_epochs 10 \
+  --save_steps 10000 \
+  --output_dir ./results
